d7387979495c05becf68d26060667424c4a2c234
Rakka Rage <rakkarage@gmail.com>
2019-02-25 17:38:29 -0500

    x

--- Assets/Uband/Loot.cs
+++ Assets/Uband/Loot.cs
@@ -1,18 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Loot.cs : MonoBehaviour
+public class Loot : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
